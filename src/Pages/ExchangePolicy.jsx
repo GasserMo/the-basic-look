66469df4c794a3eaa830223dbe0b6b567872ebf8
GasserMo <gassermohamed648@gmail.com>
2024-10-03 00:10:01 +0200

    Donee here

--- src/Pages/ExchangePolicy.jsx
+++ src/Pages/ExchangePolicy.jsx
@@ -1,9 +1,11 @@
 function ExchangePolicy() {
   return (
     <div className="text-center flex flex-col items-center ">
-      <p className="text-5xl mt-16 mb-5 font-light">EXCHANGE & REFUND POLICY</p>
+      <p className="md:text-5xl text-4xl mt-16 mb-5 font-light">
+        EXCHANGE & REFUND POLICY
+      </p>
 
-      <p className=" w-[50%] font-medium">
+      <p className=" w-[70%] font-medium">
         Thanks for shopping at thebasiclook.comIf you are not entirely satisfied
         with your purchase, we are here to help.
       </p>
@@ -11,7 +13,7 @@ function ExchangePolicy() {
         Please Contact us here →
       </p>
       <div
-        className="flex justify-center underline cursor-pointer w-[50%]
+        className="flex justify-center underline cursor-pointer w-[70%] md:w-[50%]
        hover:text-cyan-700  text-xl text-left"
       >
         ONCE YOUR REQUEST IS APPROVED, IT WILL ARRIVE IN 5 DAYS, PLEASE NOTE{" "}
